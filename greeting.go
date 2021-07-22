35376c445c474f39dd3359fa331124079f2a8222
egublog <egukei0729@gmail.com>
2021-07-22 22:35:45 +0900

    use array

--- greeting.go
+++ greeting.go
@@ -2,10 +2,15 @@ package main
 import ("fmt")
 
 func main() {
-	var num int 
-	num = 1
+	num := 3
 	fmt.Println(num)
 	fmt.Println("Good morning")
 	fmt.Println("Good afternoon")
 	fmt.Println("Good evening")
+	array()
+}
+
+func array() {
+	a := [...]string{"a", "b", "c"}
+	fmt.Println(a)
 }
