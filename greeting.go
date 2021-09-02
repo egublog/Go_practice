2b6ad03c535468ceb7adca61abd3f68d5e059b6a
egublog <egukei0729@gmail.com>
2021-09-02 21:40:02 +0900

    [TRY] コマンドライン引数

--- greeting.go
+++ greeting.go
@@ -1,5 +1,8 @@
 package main
-import ("fmt")
+import (
+	"fmt"
+	"os"
+)
 
 type User struct {
 	gender string
@@ -10,17 +13,15 @@ type User struct {
 func main() {
 	u := User{"male", 22, "東京都八王子市"}
 
-	a := 439
-	b := &a
-
-	c := []int {1, 2, 3, 4, 5, 56}
-	d := c[1:3]
+	var s, sep string
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	fmt.Println(s)
 
 	fmt.Println(u)
 	fmt.Println(u.gender)
-	fmt.Println(*b)
-	fmt.Println(c)
-	fmt.Println(d)
 
 	fmt.Println(hello())
 }
