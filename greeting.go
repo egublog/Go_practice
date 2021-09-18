34115a4e3e8494c32b4cda3abfbf77fdbdbb64c9
egublog <egukei0729@gmail.com>
2021-09-18 23:08:14 +0900

    [MODIFY] ポインタを消す

--- greeting.go
+++ greeting.go
@@ -4,23 +4,7 @@ import "fmt"
 
 
 func main() {
-	var i int = 100
-	var j int = 200
-	var p1 *int
-	var p2 *int
-	p1 = &i
-	p2 = &j
-
-	i = *p1 + *p2
-	fmt.Println(*p1)
-	i = *p1 + *p2
-	fmt.Println(*p1)
-	i = *p1 + *p2
-	fmt.Println(*p1)
-	i = *p1 + *p2
-	fmt.Println(*p1)
-	i = *p1 + *p2
-	fmt.Println(*p1)
+	fmt.Println("aa")
 }
 
 func circleArea(pai float64) func(radius float64) float64 {
