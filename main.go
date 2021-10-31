3509487a3d27569562134c3974d997096531ac32
egublog <egukei0729@gmail.com>
2021-10-31 22:19:53 +0900

    [MODIFY] h1を使ってみる

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, HTTPサーバ")
+	fmt.Fprint(w, "<h1>Hello World</h1>")
 }
 func main() {
 	http.HandleFunc("/", handler)
