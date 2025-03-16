bd6859d0448d3a40ca3b62e30fab190d10d0edcc
egublog <egukei0729@gmail.com>
2025-03-16 23:16:43 +0900

    modify: 文法エラーの修正

--- mylib/human.go
+++ mylib/human.go
@@ -1,11 +1,14 @@
 package mylib
 
-func Average(s []int) int {
-	total := 0
-
-	for _, i := range	s {
-		total += i
+func Average(numbers []int) int {
+	if len(numbers) == 0 {
+		return 0
 	}
 
-	return int(total/len(s))
+	total := 0
+	for _, num := range numbers {
+		total += num
+	}
+
+	return total / len(numbers)
 }
