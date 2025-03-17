bb7423285602e815ce3e6f1321a1fbc2429ed0aa
egublog <egukei0729@gmail.com>
2025-03-17 22:43:52 +0900

    modify: サーバーの起動を確認

--- main.go
+++ main.go
@@ -9,6 +9,10 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h2>Hello World</h2>")
 }
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+    http.HandleFunc("/", handler)
+    fmt.Println("サーバーを起動しました。http://localhost:8081 にアクセスしてください。")
+    err := http.ListenAndServe(":8081", nil)
+    if err != nil {
+        fmt.Printf("サーバーの起動に失敗しました: %v\n", err)
+    }
 }
