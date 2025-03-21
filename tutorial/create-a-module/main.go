1e7b6bc343f75f2690f51cea9fafbae60bc8b8dd
egublog <egukei0729@gmail.com>
2025-03-21 22:43:38 +0900

    add: Hello()

--- tutorial/create-a-module/main.go
+++ tutorial/create-a-module/main.go
@@ -5,5 +5,14 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Call the Hello function from the greeting package.
+	message := Hello("Gopher")
+	fmt.Println(message)
+}
+
+// Hello returns a greeting for the named person.
+func Hello(name string) string {
+	// Return a greeting that embeds the name in a message.
+	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	return message
 }
