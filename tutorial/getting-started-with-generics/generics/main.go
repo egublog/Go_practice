937f4977038e86448fcf837d849c307f3fc6858d
egublog <egukei0729@gmail.com>
2025-03-25 00:57:13 +0900

    add: main

--- tutorial/getting-started-with-generics/generics/main.go
+++ tutorial/getting-started-with-generics/generics/main.go
@@ -1,5 +1,25 @@
 package main
 
+import "fmt"
+
+func main() {
+	// Initialize a map for the integer values
+	ints := map[string]int64{
+			"first":  34,
+			"second": 12,
+	}
+
+	// Initialize a map for the float values
+	floats := map[string]float64{
+			"first":  35.98,
+			"second": 26.99,
+	}
+
+	fmt.Printf("Non-Generic Sums: %v and %v\n",
+			SumInts(ints),
+			SumFloats(floats))
+}
+
 // SumInts adds together the values of m.
 func SumInts(m map[string]int64) int64 {
 	var s int64
