63d6e6ccba4922dacee333692c6d664ea99954d9
egublog <egukei0729@gmail.com>
2025-03-27 00:08:35 +0900

    modify: Reverse()

--- tutorial/getting-started-with-fuzzing/fuzz/main.go
+++ tutorial/getting-started-with-fuzzing/fuzz/main.go
@@ -10,11 +10,12 @@ func main() {
 	fmt.Printf("reversed: %q\n", rev)
 	fmt.Printf("reversed again: %q\n", doubleRev)
 }
-
+ 
 func Reverse(s string) string {
-	b := []byte(s)
-	for i, j := 0, len(b)-1; i < len(b)/2; i, j = i+1, j-1 {
-			b[i], b[j] = b[j], b[i]
+	r := []rune(s)
+	for i := range len(r)/2 {
+		j := len(r) - i - 1
+		r[i], r[j] = r[j], r[i]
 	}
-	return string(b)
+	return string(r)
 }
