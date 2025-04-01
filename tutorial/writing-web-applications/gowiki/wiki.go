2339e93baa9cfa260107c6eaeaa0587ce8fac70b
egublog <egukei0729@gmail.com>
2025-04-01 23:40:49 +0900

    modify: net/http

--- tutorial/writing-web-applications/gowiki/wiki.go
+++ tutorial/writing-web-applications/gowiki/wiki.go
@@ -1,32 +1,16 @@
 package main
 
 import (
-	"fmt"
-	"os"
+    "fmt"
+    "log"
+    "net/http"
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
 func main() {
-	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-	p1.save()
-	p2, _ := loadPage("TestPage")
-	fmt.Println(string(p2.Body))
-}
-
-type Page struct {
-	Title string
-	Body  []byte
-}
-
-func (p *Page) save() error {
-	filename := p.Title + ".txt"
-	return os.WriteFile(filename, p.Body, 0600)
-}
-
-func loadPage(title string) (*Page, error) {
-	filename := title + ".txt"
-	body, err := os.ReadFile(filename)
-	if err != nil {
-			return nil, err
-	}
-	return &Page{Title: title, Body: body}, nil
+    http.HandleFunc("/", handler)
+    log.Fatal(http.ListenAndServe(":8080", nil))
 }
