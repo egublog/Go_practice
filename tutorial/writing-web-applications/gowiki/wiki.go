99324fb64bbf6fe779e90db7ca3c6e7cd6d329c8
egublog <egukei0729@gmail.com>
2025-04-03 23:56:12 +0900

    add: editHandler

--- tutorial/writing-web-applications/gowiki/wiki.go
+++ tutorial/writing-web-applications/gowiki/wiki.go
@@ -9,6 +9,7 @@ import (
 
 func main() {
 	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
@@ -30,4 +31,18 @@ func loadPage(title string) (*Page, error) {
 			return nil, err
 	}
 	return &Page{Title: title, Body: body}, nil
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+			p = &Page{Title: title}
+	}
+	fmt.Fprintf(w, "<h1>Editing %s</h1>"+
+			"<form action=\"/save/%s\" method=\"POST\">"+
+			"<textarea name=\"body\">%s</textarea><br>"+
+			"<input type=\"submit\" value=\"Save\">"+
+			"</form>",
+			p.Title, p.Title, p.Body)
 }
